41259e3ae6cf77999226c3026987d87b855f5eb7
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-09-06 18:08:15 +0300

    initialization

---
+++ LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/LaundrySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.LaundrySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundrySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundrySystemApplication.class, args);
+	}
+
+}
---
+++ LaundrySystem-Backend/src/test/java/com/example/LaundrySystem/LaundrySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LaundrySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundrySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
