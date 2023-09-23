d8e64027d72109843aff3c90ed2ed796a72ed66a
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-09-23 04:06:33 +0300

    horizontal to vertical + some styling

--- LaundrySystem-Frontend/src/components/NavBar.tsx
+++ LaundrySystem-Frontend/src/components/NavBar.tsx
@@ -1,52 +1,157 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faFilePen,
+  faListCheck,
+  faIdCard,
+  faUsersGear,
+  faChartLine,
+  faLeftLong,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface NavBarProps {
   employeeName: string;
   laundryName: string;
+  containerPage: string;
 }
 
-const NavBar = ({ employeeName, laundryName }: NavBarProps) => {
+const NavBar = ({ employeeName, laundryName, containerPage }: NavBarProps) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+    <div
+      className="d-flex flex-column justify-content-between bg-primary text-light"
+      style={{ height: "100vh", width: "25%" }}
+    >
+      {/* Section 1: Laundry and Employee Name */}
+      <div className="p-3">
+        <div
+          className="mb-3"
+          style={{ fontSize: "18px", color: "black", fontWeight: "bold" }}
+        >
+          <img
+            src="../../public/washing-machine.png"
+            alt="Laundry Icon"
+            className="mr-2"
+            style={{ marginRight: "8px" }}
+            width="45"
+            height="45"
+          />
           {laundryName}
-        </Link>
-
-        <div className="navbar-text">{employeeName}</div>
-
-        <div className="ml-auto">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/orders">
-                Orders
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/statistics">
-                Statistics
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/tasks">
-                Employee Tasks
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/customers">
-                Customers Management
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/employees">
-                Employees Management
-              </Link>
-            </li>
-          </ul>
+        </div>
+        <div style={{ fontSize: "18px", fontWeight: "bold", color: "black" }}>
+          <FontAwesomeIcon
+            icon={faUser}
+            className="fs-3 mr-2"
+            style={{ marginRight: "8px" }}
+          />
+          {employeeName}
         </div>
       </div>
-    </nav>
+
+      {/* Section 2: Buttons with Links */}
+      <div
+        className="d-flex flex-column p-2"
+        // style={{ backgroundColor: "#457cba" }}
+      >
+        <Link
+          to="/orders"
+          className="btn m-0 p-2"
+          style={{
+            textAlign: "left",
+            ...(containerPage === "orders" && { backgroundColor: "#73b0f5" }),
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faFilePen}
+            className="fs-4 mr-2"
+            style={{ marginRight: "15px" }}
+          />
+          Orders
+        </Link>
+        <Link
+          to="/tasks"
+          className="btn m-0 p-2"
+          style={{
+            textAlign: "left",
+            ...(containerPage === "empTasks" && { backgroundColor: "#73b0f5" }),
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faListCheck}
+            className="fs-4 mr-2"
+            style={{ marginRight: "15px" }}
+          />
+          Employees Tasks
+        </Link>
+        <Link
+          to="/employees"
+          className="btn m-0 p-2"
+          style={{
+            textAlign: "left",
+            ...(containerPage === "empManagement" && {
+              backgroundColor: "#73b0f5",
+            }),
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faUsersGear}
+            className="fs-4 mr-2"
+            style={{ marginRight: "15px" }}
+          />
+          Employees Management
+        </Link>
+        <Link
+          to="/customers"
+          className="btn m-0 p-2"
+          style={{
+            textAlign: "left",
+            ...(containerPage === "customerManagement" && {
+              backgroundColor: "#73b0f5",
+            }),
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faIdCard}
+            className="fs-4 mr-2"
+            style={{ marginRight: "15px" }}
+          />
+          Customers Management
+        </Link>
+        <Link
+          to="/statistics"
+          className="btn m-0 p-2"
+          style={{
+            textAlign: "left",
+            ...(containerPage === "statistics" && {
+              backgroundColor: "#73b0f5",
+            }),
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faChartLine}
+            className="fs-4 mr-2"
+            style={{ marginRight: "15px" }}
+          />
+          Statistics
+        </Link>
+      </div>
+
+      {/* Section 3: Logout Button */}
+      <div
+        className="d-flex flex-column align-items-center p-3"
+        style={{ marginBottom: "10vh" }}
+      >
+        <Link to="/empSignin" className="btn btn-danger mb-2">
+          <FontAwesomeIcon
+            icon={faLeftLong}
+            className="fs-4 mr-2"
+            style={{ marginRight: "10px" }}
+          />
+          Logout
+        </Link>
+      </div>
+    </div>
   );
 };
 
