cfb4564583157d6e3dc0e93066b41038cf4cad11
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-09-23 04:10:16 +0300

    to send orders with items to the front

---
+++ LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Entities/OrderItemPair.java
@@ -0,0 +1,32 @@
+package com.example.LaundrySystem.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
+
+public class OrderItemPair {
+    private Order order;
+    private List<OrderItem> orderItems;
+
+    public OrderItemPair(Order order, List<OrderItem> orderItems) {
+        this.order = order;
+        this.orderItems = orderItems;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+}
