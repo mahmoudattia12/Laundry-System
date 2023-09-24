b8bad01ff058fea85517d08657d7c061bc9cec31
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-09-24 05:25:34 +0300

    update order repo (considering multiple laundries)

--- LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Repositories/OrderRepository.java
+++ LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Repositories/OrderRepository.java
@@ -18,81 +18,177 @@ public interface OrderRepository<T extends Comparable<T>> extends JpaRepository<
 //    @Query("SELECT new com.example.LaundrySystem.Repositories.OrderWithItemsDTO(o.id, o.currState) FROM Order o")
 //    List<OrderWithItemsDTO> findAllOrdersWithItems();
 
-    @Query("SELECT new com.example.LaundrySystem.Entities.OrderItemPair(o, oi) FROM Order o JOIN o.items oi")
-    List<OrderItemPair> findAllOrdersWithItems();
+//    @Query("SELECT new com.example.LaundrySystem.Entities.OrderItemPair(o, oi) FROM Order o JOIN o.items oi")
+//    List<OrderItemPair> findAllOrdersWithItems();
 
-    //sort
-    List<Order> findAllByOrderByIDAsc();
+//    //sort
+//    List<Order> findAllByOrderByIDAsc();
+//
+//    List<Order> findAllByOrderByIDDesc();
+//
+//    List<Order> findAllByOrderByCustomerPhoneNumberAsc();
+//
+//    List<Order> findAllByOrderByCustomerPhoneNumberDesc();
+//
+//    List<Order> findAllByOrderByCustomerNameAsc();
+//
+//    List<Order> findAllByOrderByCustomerNameDesc();
+//
+//    List<Order> findAllByOrderByAlternatePhoneAsc();
+//
+//    List<Order> findAllByOrderByAlternatePhoneDesc();
+//
+//    List<Order> findAllByOrderByCurrStateAsc();
+//
+//    List<Order> findAllByOrderByCurrStateDesc();
+//
+//    List<Order> findAllByOrderByStartDateAsc();
+//
+//    List<Order> findAllByOrderByStartDateDesc();
+//
+//    List<Order> findAllByOrderByEndDateAsc();
+//
+//    List<Order> findAllByOrderByEndDateDesc();
+//
+//    List<Order> findAllByOrderByTotalPriceAsc();
+//
+//    List<Order> findAllByOrderByTotalPriceDesc();
+//
+//    //filter
+//    @Query("SELECT o FROM Order o WHERE CAST(o.ID AS string) = :ID")
+//    List<Order> findByID(@Param("ID") String ID);
+//
+//    List<Order> findByCurrState(String currState);
+//
+//    @Query("SELECT o FROM Order o WHERE CAST(o.isDelivery AS string) = :isDelivery")
+//    List<Order> findByIsDelivery(@Param("isDelivery") String isDelivery);
+//
+//    @Query("SELECT o FROM Order o WHERE CAST(o.isPaid AS string) = :isPaid")
+//    List<Order> findByIsPaid(@Param("isPaid") String isPaid);
+//
+//    @Query("SELECT o FROM Order o WHERE CAST(o.startDate AS string) = :startDate")
+//    List<Order> findByStartDate(@Param("startDate") String startDate);
+//
+//    @Query("SELECT o FROM Order o WHERE CAST(o.endDate AS string) = :endDate")
+//    List<Order> findByEndDate(@Param("endDate") String endDate);
+//
+//    //    @Query("SELECT o FROM Order o WHERE CAST(o.totalPrice AS string) = :totalPrice")
+////    List<Order> findByTotalPrice(@Param("totalPrice")String totalPrice);
+//    List<Order> findByTotalPrice(double totalPrice);
+//
+//    List<Order> findByAlternatePhone(String alternatePhone);
+//
+//    List<Order> findByCustomerPhoneNumber(String customerPhoneNumber);
+//    List<Order> findByCustomerName(String customerName);
+//
+//    //search
+//    @Query("SELECT o FROM Order o WHERE " +
+//            "CAST(o.ID AS string) LIKE %:partialInput% OR " +
+//            "o.currState LIKE %:partialInput% OR " +
+//            "o.alternatePhone LIKE %:partialInput% OR " +
+//            "o.customer.phoneNumber LIKE %:partialInput% OR " +
+//            "o.customer.name LIKE %:partialInput% OR " +
+//            "CAST(o.totalPrice AS string) LIKE %:partialInput% OR " +
+//            "CAST(o.isDelivery AS string) LIKE %:partialInput% OR " +
+//            "CAST(o.isPaid AS string) LIKE %:partialInput% OR " +
+//            "CAST(o.startDate AS string) LIKE %:partialInput% OR " +
+//            "CAST(o.endDate AS string) LIKE %:partialInput%")
+//    List<Order> findByAttributesContaining(@Param("partialInput") String partialInput);
+//
+////    @Query("SELECT o FROM Order o WHERE " +
+////            "CAST(o.ID AS string) LIKE %:partialInput% OR " +
+////            "o.currState LIKE %:partialInput% OR " +
+////            "o.alternatePhone LIKE %:partialInput% OR " +
+////            "o.customer.phoneNumber LIKE %:partialInput% OR " +
+////            "o.totalPrice LIKE %:number% OR " +
+////            "CAST(o.isDelivery AS string) LIKE %:partialInput% OR " +
+////            "CAST(o.startDate AS string) LIKE %:partialInput% OR " +
+////            "CAST(o.endDate AS string) LIKE %:partialInput%")
+////    List<Order> findByAttributesContaining(@Param("partialInput") String partialInput, @Param("number") double number);
 
-    List<Order> findAllByOrderByIDDesc();
+    // Sort orders for a specific laundry
 
-    List<Order> findAllByOrderByCustomerPhoneNumberAsc();
+    List<Order> findAllByLaundryNameOrderByIDAsc(String laundryName);
 
-    List<Order> findAllByOrderByCustomerPhoneNumberDesc();
+    List<Order> findAllByLaundryNameOrderByIDDesc(String laundryName);
 
-    List<Order> findAllByOrderByAlternatePhoneAsc();
+    List<Order> findAllByLaundryNameOrderByCustomerPhoneNumberAsc(String laundryName);
 
-    List<Order> findAllByOrderByAlternatePhoneDesc();
+    List<Order> findAllByLaundryNameOrderByCustomerPhoneNumberDesc(String laundryName);
 
-    List<Order> findAllByOrderByCurrStateAsc();
+    List<Order> findAllByLaundryNameOrderByCustomerNameAsc(String laundryName);
 
-    List<Order> findAllByOrderByCurrStateDesc();
+    List<Order> findAllByLaundryNameOrderByCustomerNameDesc(String laundryName);
 
-    List<Order> findAllByOrderByStartDateAsc();
+    List<Order> findAllByLaundryNameOrderByAlternatePhoneAsc(String laundryName);
 
-    List<Order> findAllByOrderByStartDateDesc();
+    List<Order> findAllByLaundryNameOrderByAlternatePhoneDesc(String laundryName);
 
-    List<Order> findAllByOrderByEndDateAsc();
+    List<Order> findAllByLaundryNameOrderByCurrStateAsc(String laundryName);
 
-    List<Order> findAllByOrderByEndDateDesc();
+    List<Order> findAllByLaundryNameOrderByCurrStateDesc(String laundryName);
 
-    List<Order> findAllByOrderByTotalPriceAsc();
+    List<Order> findAllByLaundryNameOrderByStartDateAsc(String laundryName);
 
-    List<Order> findAllByOrderByTotalPriceDesc();
+    List<Order> findAllByLaundryNameOrderByStartDateDesc(String laundryName);
 
-    //filter
-    @Query("SELECT o FROM Order o WHERE CAST(o.ID AS string) = :ID")
-    List<Order> findByID(@Param("ID") String ID);
+    List<Order> findAllByLaundryNameOrderByEndDateAsc(String laundryName);
 
-    List<Order> findByCurrState(String currState);
+    List<Order> findAllByLaundryNameOrderByEndDateDesc(String laundryName);
 
-    @Query("SELECT o FROM Order o WHERE CAST(o.isDelivery AS string) = :isDelivery")
-    List<Order> findByIsDelivery(@Param("isDelivery") String isDelivery);
+    List<Order> findAllByLaundryNameOrderByTotalPriceAsc(String laundryName);
 
-    @Query("SELECT o FROM Order o WHERE CAST(o.startDate AS string) = :startDate")
-    List<Order> findByStartDate(@Param("startDate") String startDate);
+    List<Order> findAllByLaundryNameOrderByTotalPriceDesc(String laundryName);
+    List<Order> findAllByLaundryNameOrderByIsDeliveryAsc(String laundryName);
 
-    @Query("SELECT o FROM Order o WHERE CAST(o.endDate AS string) = :endDate")
-    List<Order> findByEndDate(@Param("endDate") String endDate);
+    List<Order> findAllByLaundryNameOrderByIsDeliveryDesc(String laundryName);
+    List<Order> findAllByLaundryNameOrderByIsPaidAsc(String laundryName);
 
-    //    @Query("SELECT o FROM Order o WHERE CAST(o.totalPrice AS string) = :totalPrice")
-//    List<Order> findByTotalPrice(@Param("totalPrice")String totalPrice);
-    List<Order> findByTotalPrice(double totalPrice);
+    List<Order> findAllByLaundryNameOrderByIsPaidDesc(String laundryName);
 
-    List<Order> findByAlternatePhone(String alternatePhone);
+    // Filter orders for a specific laundry
 
-    List<Order> findByCustomerPhoneNumber(String customerPhoneNumber);
+    @Query("SELECT o FROM Order o WHERE o.laundry.name = :laundryName AND CAST(o.ID AS string) = :ID")
+    List<Order> findByIDAndLaundryName(@Param("ID") String ID, @Param("laundryName") String laundryName);
 
-    //search
+    List<Order> findByCurrStateAndLaundryName(String currState, String laundryName);
+
+    @Query("SELECT o FROM Order o WHERE o.laundry.name = :laundryName AND CAST(o.isDelivery AS string) = :isDelivery")
+    List<Order> findByIsDeliveryAndLaundryName(@Param("isDelivery") String isDelivery, @Param("laundryName") String laundryName);
+
+    @Query("SELECT o FROM Order o WHERE o.laundry.name = :laundryName AND CAST(o.isPaid AS string) = :isPaid")
+    List<Order> findByIsPaidAndLaundryName(@Param("isPaid") String isPaid, @Param("laundryName") String laundryName);
+
+    @Query("SELECT o FROM Order o WHERE o.laundry.name = :laundryName AND CAST(o.startDate AS string) = :startDate")
+    List<Order> findByStartDateAndLaundryName(@Param("startDate") String startDate, @Param("laundryName") String laundryName);
+
+    @Query("SELECT o FROM Order o WHERE o.laundry.name = :laundryName AND CAST(o.endDate AS string) = :endDate")
+    List<Order> findByEndDateAndLaundryName(@Param("endDate") String endDate, @Param("laundryName") String laundryName);
+
+    List<Order> findByTotalPriceAndLaundryName(double totalPrice, String laundryName);
+
+    List<Order> findByAlternatePhoneAndLaundryName(String alternatePhone, String laundryName);
+
+    List<Order> findByCustomerPhoneNumberAndLaundryName(String customerPhoneNumber, String laundryName);
+
+    List<Order> findByCustomerNameAndLaundryName(String customerName, String laundryName);
+
+    // Search orders by attributes and laundry name
+
     @Query("SELECT o FROM Order o WHERE " +
+            "o.laundry.name = :laundryName AND (" +
             "CAST(o.ID AS string) LIKE %:partialInput% OR " +
             "o.currState LIKE %:partialInput% OR " +
             "o.alternatePhone LIKE %:partialInput% OR " +
             "o.customer.phoneNumber LIKE %:partialInput% OR " +
+            "o.customer.name LIKE %:partialInput% OR " +
             "CAST(o.totalPrice AS string) LIKE %:partialInput% OR " +
             "CAST(o.isDelivery AS string) LIKE %:partialInput% OR " +
+            "CAST(o.isPaid AS string) LIKE %:partialInput% OR " +
             "CAST(o.startDate AS string) LIKE %:partialInput% OR " +
-            "CAST(o.endDate AS string) LIKE %:partialInput%")
-    List<Order> findByAttributesContaining(@Param("partialInput") String partialInput);
-
-    @Query("SELECT o FROM Order o WHERE " +
-            "CAST(o.ID AS string) LIKE %:partialInput% OR " +
-            "o.currState LIKE %:partialInput% OR " +
-            "o.alternatePhone LIKE %:partialInput% OR " +
-            "o.customer.phoneNumber LIKE %:partialInput% OR " +
-            "o.totalPrice LIKE %:number% OR " +
-            "CAST(o.isDelivery AS string) LIKE %:partialInput% OR " +
-            "CAST(o.startDate AS string) LIKE %:partialInput% OR " +
-            "CAST(o.endDate AS string) LIKE %:partialInput%")
-    List<Order> findByAttributesContaining(@Param("partialInput") String partialInput, @Param("number") double number);
+            "CAST(o.endDate AS string) LIKE %:partialInput%)")
+    List<Order> findByAttributesContainingAndLaundryName(
+            @Param("partialInput") String partialInput,
+            @Param("laundryName") String laundryName
+    );
 }
