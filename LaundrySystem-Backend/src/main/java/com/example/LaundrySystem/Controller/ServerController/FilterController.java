f59a29004ec95e3ecab327b618357d7fce0f1997
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-09-24 05:27:50 +0300

    update order sorting & filter & search controller

--- LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Controller/ServerController/FilterController.java
+++ LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Controller/ServerController/FilterController.java
@@ -2,6 +2,7 @@ package com.example.LaundrySystem.Controller.ServerController;
 
 import com.example.LaundrySystem.Controller.Filter.FilterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5173/")
 public class FilterController <T extends Comparable<T>> {
     @Autowired
     FilterService filterService;
 
-    @GetMapping("/filterEntity/{entity}/{criteria}/{toMeet}")
-    public List<T> filterEntity(@PathVariable String entity, @PathVariable String criteria, @PathVariable String toMeet){
-        return filterService.filter(entity, criteria, toMeet);
+    @GetMapping("/filterEntity/{entity}/{criteria}/{toMeet}/{laundryName}")
+    public List<T> filterEntity(@PathVariable String entity, @PathVariable String criteria, @PathVariable String toMeet, @PathVariable String laundryName){
+        return filterService.filter(entity, criteria, toMeet, laundryName);
     }
 }
--- LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Controller/ServerController/SortingController.java
+++ LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Controller/ServerController/SortingController.java
@@ -2,19 +2,17 @@ package com.example.LaundrySystem.Controller.ServerController;
 
 import com.example.LaundrySystem.Controller.Sorting.SortServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5173/")
 public class SortingController <T extends Comparable<T>> {
     @Autowired
     SortServices sortServices;
-    @GetMapping("/sortEntity/{entity}/{sortBy}/{order}")
-    public List<T> sortEntity(@PathVariable String entity, @PathVariable String sortBy, @PathVariable boolean order){
-        return sortServices.sort(entity, sortBy, order);
+    @GetMapping("/sortEntity/{entity}/{sortBy}/{order}/{laundryName}")
+    public List<T> sortEntity(@PathVariable String entity, @PathVariable String sortBy, @PathVariable boolean order, @PathVariable String laundryName){
+        return sortServices.sort(entity, sortBy, order, laundryName);
     }
 }
