5d822c82115774fb4072af65c01afce90854092b
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-09-28 00:03:42 +0300

    edit repo

--- LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Repositories/CustomerRepository.java
+++ LaundrySystem-Backend/src/main/java/com/example/LaundrySystem/Repositories/CustomerRepository.java
@@ -12,38 +12,6 @@ import java.util.List;
 
 @Repository
 public interface CustomerRepository<T extends Comparable<T>> extends JpaRepository<Customer, String> {
-//    //sort
-//    List<Customer> findAllByLaundryNameOrderByNameAsc(String laundryName);
-//    List<Customer> findAllByLaundryNameOrderByNameDesc(String laundryName);
-//    List<Customer> findAllByLaundryNameOrderByEmailAsc(String laundryName);
-//    List<Customer> findAllByLaundryNameOrderByEmailDesc(String laundryName);
-//    List<Customer> findAllByLaundryNameOrderByPhoneNumberAsc(String laundryName);
-//    List<Customer> findAllByLaundryNameOrderByPhoneNumberDesc(String laundryName);
-//    List<Customer> findAllByLaundryNameOrderByAddressAsc(String laundryName);
-//    List<Customer> findAllByLaundryNameOrderByAddressDesc(String laundryName);
-////    List<Customer> findAllByOrderByTotalPaysAsc();
-////    List<Customer> findAllByOrderByTotalPaysDesc();
-//
-//    //filter
-//    List<Customer> findByPhoneNumberAndLaundryName(String phoneNumber, String laundryName);
-//    List<Customer> findByEmailAndLaundryName(String email, String laundryName);
-//    List<Customer> findByNameAndLaundryName(String name, String laundryName);
-//    List<Customer> findByAddressAndLaundryName(String address, String laundryName);
-////    @Query("SELECT c FROM Customer c WHERE CAST(c.isGoldCustomer AS string) = :isGoldCustomer")
-////    List<Customer> findByIsGoldCustomer(@Param("isGoldCustomer") String isGoldCustomer);
-////    //@Query("SELECT c FROM Customer c WHERE CAST(c.totalPays AS string) = :totalPays")
-////    //List<Customer> findByTotalPays(@Param("totalPays") String totalPays);
-////    List<Customer> findByTotalPays(double totalPays);
-//
-//    //search
-////    @Query("SELECT c FROM Customer c WHERE " +
-////            "c.name LIKE %:partialInput% OR " +
-////            "c.email LIKE %:partialInput% OR " +
-////            "c.phoneNumber LIKE %:partialInput% OR " +
-////            "c.address LIKE %:partialInput% OR " +
-////            "CAST(c.isGoldCustomer AS string) LIKE %:partialInput% OR " +
-////            "c.totalPays = :number")
-////    List<Customer> findByAttributesContaining(@Param("partialInput") String partialInput, @Param("number") double number);
 
     List<Customer> findAllByLaundries_Name(String laundryName, Sort sort);
 
@@ -54,16 +22,7 @@ public interface CustomerRepository<T extends Comparable<T>> extends JpaReposito
     List<Customer> findByLaundries_NameAndPhoneNumber(String laundryName, String phoneNumber);
 
     List<Customer> findByLaundries_NameAndAddress(String laundryName, String address);
-//    @Query("SELECT c FROM Customer c WHERE " +
-//            "c.laundry.name = :laundryName AND (" +
-//            "c.name LIKE %:partialInput% OR " +
-//            "c.email LIKE %:partialInput% OR " +
-//            "c.phoneNumber LIKE %:partialInput% OR " +
-//            "c.address LIKE %:partialInput%)" )
-//
-//    List<Customer> findByAttributesContainingAndLaundryName(@Param("laundryName") String laundryName, @Param("partialInput") String partialInput);
 
-// Search customers by partial input in name, email, or phone number for a given laundryName
     @Query("SELECT c FROM Customer c JOIN c.laundries l WHERE l.name = :laundryName AND " +
             "(c.name LIKE %:partialInput% OR c.address LIKE %:partialInput% OR c.email LIKE %:partialInput% OR c.phoneNumber LIKE %:partialInput%)")
     List<Customer> findByAttributesContainingAndLaundryName(@Param("laundryName") String laundryName, @Param("partialInput") String partialInput);
